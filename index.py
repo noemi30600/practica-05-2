b99f6abd63f1f62cdea8ba69d3f353f05db7e699
MrFantastic07 <jaredavila.cruz2001@gmail.com>
2023-11-19 18:06:38 -0600

    Inicio del proyecto

---
+++ index.py
@@ -0,0 +1,15 @@
+# Menú de operaciones
+print("Seleccione la operación:")
+print("1. Suma")
+print("2. Producto")
+print("3. Division")
+print("4. Factorial")
+print("5. Tablas de multiplicar")
+print("6. Calculo de cuadrado y cubo")
+print("7. Promedio")
+print("8. Maximo y minimo")
+
+# Solicitar al usuario la elección
+opcion = int(input("Ingrese el número de la operación deseada: "))
+
+
